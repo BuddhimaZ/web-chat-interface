010739c9bc2c08f764376f46d154059a9a00b7bc
Buddhima Zoysa <buddhima.zoysa@arimaclanka.com>
2025-10-13 18:57:35 +0530

    Undid the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
-import Chat from './components/Chat'
-import { PipecatClient } from "@pipecat-ai/client-js";
-import { PipecatClientProvider } from "@pipecat-ai/client-react";
-import { WebSocketTransport } from "@pipecat-ai/websocket-transport";
-
-const client = new PipecatClient({
-  transport: new WebSocketTransport(),
-  enableMic: false,
-  enableCam: false,
-  enableScreenShare: false,
-});
-
 function App() {
   return (
-    <PipecatClientProvider client={client}>
-      <Chat />
-    </PipecatClientProvider>
+    <></>
   );
 }
 
